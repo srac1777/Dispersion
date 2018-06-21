fee8ff58d49c6590063c587e527900b7ccf0e058
Shashank Racherla <shashank.racherla@gmail.com>
2018-06-21 09:24:22 -0700

    before user input RB colors only

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,7 +3,7 @@ let ctx = canvas.getContext('2d');
 let mouseX;
 let mouseY;
 let k = 0;
-let cursor_radius = 30;
+let cursor_radius = 140;
 let particles_array = [];
 let particle = {vx: 0, vy: 0, x: 0, y: 0 };
 let p;
@@ -16,12 +16,12 @@ let changed_particles_array = [];
 function setup(){
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-    for (i = 0; i < 120; i += 1) {
-        for (j = 0; j < 60; j += 1) {
+    for (i = 0; i < 50; i += 1) {
+        for (j = 0; j < 50; j += 1) {
             
-            let i1 = 50 + i * 4;
+            let i1 = 100 + i * 11;
 
-            let j1 = 25 + j * 4;
+            let j1 = 100 + j * 11;
             
             p = Object.create(particle);
             p.x = i1;
@@ -29,10 +29,37 @@ function setup(){
             p.y = j1;
             p.original_y = j1;
             particles_array[k] = p;
-            p.changed = false;
+            p.motion = "static";
             k += 1; 
+
         }
     }
+
+    for (i = 0; i < 50; i += 1) {
+        for (j = 0; j < 50; j += 1) {
+
+            let i1 = 101 + i * 11;
+
+            let j1 = 100 + j * 11;
+
+            p = Object.create(particle);
+            p.x = i1;
+            p.original_x = i1;
+            p.y = j1;
+            p.original_y = j1;
+            particles_array[k] = p;
+            p.motion = "static";
+            k += 1;
+
+        }
+    }
+
+
+
+
+
+
+
 
     // for (let k = 0; k < particles_array.length; k++) {
     //     // debugger
@@ -69,22 +96,20 @@ function update(){
             theta = Math.atan2(dy, dx);
             p.vx += force * Math.cos(theta);
             p.vy += force * Math.sin(theta);
-            p.changed = true
-            // changed_particles_array.push(p)
+            p.motion = "dynamic";
         }
         p.vx *= 0.9;
         p.vy *= 0.9;
-        // if(p.vx < 0.001 || p.vy < 0.001){
-        //     p.changed = false
-        //     // changed_particles_array.delete(p)
+        // if(p.vx < 0.001 ){
+        //     p.motion = "static";
         // }
         p.x += p.vx + (p.original_x - p.x) * 0.25
         p.y += p.vy + (p.original_y - p.y) * 0.25
-        ctx.beginPath();
-        ctx.arc(p.x, p.y, 1, 0, Math.PI * 2, true);
-        ctx.closePath();
-        ctx.fillStyle = "white";
-        ctx.fill();
+        // ctx.beginPath();
+        // ctx.arc(p.x, p.y, 1, 0, Math.PI * 2, true);
+        // ctx.closePath();
+        // ctx.fillStyle = "white";
+        // ctx.fill();
         
     }
     draw();
@@ -94,24 +119,26 @@ function update(){
 
 
 function draw() {
-
     ctx.clearRect(0,0,canvas.width, canvas.height);
     ctx.fillStyle = "black"
     ctx.fillRect(0,0,canvas.width, canvas.height);
 
     for (let k = 0; k < particles_array.length; k++) {
         p = particles_array[k];
-        if (p.changed === true) {
-            
-        
-            ctx.beginPath();
-            ctx.arc(p.x, p.y, 1, 0, Math.PI * 2, true);
-            ctx.closePath();
-            ctx.fillStyle = "white";
-            ctx.fill();
-        }  
-        
-        
+        // if(p.motion === "dynamic"){
+        ctx.beginPath();
+        ctx.arc(p.x, p.y, 3, 0, Math.PI * 2, true);
+        ctx.closePath();
+        if(k > 2500){
+            // if(p.x < 350 && p.y < 350){
+
+                ctx.fillStyle = "blue"
+            // }
+        } else {
+            ctx.fillStyle = "red";
+        } 
+        ctx.fill();
+        // }
     }
     
 }
